a782c6908cb9048f24e3f2cbdb5ffa2d2146a980
vinicius-silva04 <vjsilva.cic@uesc.br>
2025-07-05 16:42:08 -0300

    Update lista_com_vetor.h

--- Implementacoes/Vinicius/lista_com_vetor.h
+++ Implementacoes/Vinicius/lista_com_vetor.h
@@ -3,39 +3,38 @@
 
 #include <stdbool.h>
 
-#define MAX 1000
-
 /**
  * @struct Lista
- * @brief  Lista de inteiros com tamanho máximo definido por MAX.
- * 
+ * @brief  Lista dinâmica de inteiros com tamanho máximo definido por MAX.
+ *
  * Campos:
  * - itens: array que armazena os valores inteiros
  * - tam: quantidade atual de elementos na lista
  */
 typedef struct{
-    int itens[MAX];
+    int *itens;
     int tam;
+    int capacidade;
 }Lista;
 
 /**
  * @brief Inicializa a lista tornando seu tamanho zero.
- * 
+ *
  * @param lista ponteiro para a estrutura que será inicializada.
  */
 void inicializar(Lista *lista);
 
 /**
  * @brief Insere um valor no final da lista, se houver espaço.
- * 
+ *
  * @param lista Ponteiro para a estrutura do tipo Lista onde o valor será adicionado.
- * @param valor Valor inteiro que será adicionado. 
+ * @param valor Valor inteiro que será adicionado.
  */
 void inserir(Lista *lista,int valor);
 
 /**
  * @brief Exclui o primeiro valor indicado no parâmetro.
- * 
+ *
  * @param lista Ponteiro para a lista onde o valor será excluido.
  * @param valor Valor inteiro que será excluido.
  */
@@ -43,7 +42,7 @@ void excluir(Lista *lista,int valor);
 
 /**
  * @brief Busca um valor na lista.
- * 
+ *
  * @param lista Ponteiro da lista onde será buscado.
  * @param valor Valor inteiro que será buscado.
  * @return true se o valor existir na lista; senão false.
@@ -51,8 +50,15 @@ void excluir(Lista *lista,int valor);
 bool buscar(Lista *lista,int valor);
 
 /**
+ * @brief Libera o array alocado e muda os campos da lista para um padrão.
+ *
+ * @param lista Ponteiro da lista que será liberada.
+ */
+void libera_lista(Lista *lista);
+
+/**
  * @brief Exibe todos os elementos da lista.
- * 
+ *
  * @param lista Ponteiro da lista que será exibida.
  */
 void exibirLista(Lista *lista);
